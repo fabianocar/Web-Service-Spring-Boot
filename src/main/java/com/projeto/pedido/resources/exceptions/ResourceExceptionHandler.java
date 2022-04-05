489c8115ccab6e122228203b1f48bb877f2dcbe3
Fabianocar <sl.biano28@gmail.com>
2022-04-05 13:35:27 -0300

    Exception handling - delete

--- src/main/java/com/projeto/pedido/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projeto/pedido/resources/exceptions/ResourceExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.projeto.pedido.services.exceptions;
+package com.projeto.pedido.resources.exceptions;
 
 import java.time.Instant;
 
@@ -9,7 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.projeto.pedido.resources.exceptions.StandardError;
+import com.projeto.pedido.services.exceptions.DatabaseException;
+import com.projeto.pedido.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -18,6 +19,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		}
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 		}
